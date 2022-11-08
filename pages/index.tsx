301c7deead4c3034c5b5ee1fd0fff0e67ad2d733
Anthony <anthony.denis.becode@gmail.com>
2022-11-09 00:29:02 +0100

    front

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
-
+import{AiFillTwitterCircle,AiFillLinkedin,AiFillGithub,} from "react-icons/ai";
 
 export default function Home() {
   return (
@@ -11,17 +11,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className='bg-purple-700 px-10 '>
         <section className=" bg-purple-700 min-h-screen">
-          <nav>
-            <h1> Developed by Anthxny </h1>
-            <ul>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-burtons'> Developed by Anthxny </h1>
+            <ul className='flex items-center'>
               <li>
-                <BsFillMoonStarsFill />
+                <BsFillMoonStarsFill className='cursor-pointer text-2xl'/>
               </li>
-              <li><a href="#">Resume</a></li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
             </ul>
           </nav>
+          <div>
+            <h2>Anthony Denis</h2>
+            <h3>Developer and designer.</h3>
+            <p>Freelancer provinding services for programming and design content needs. Join me down below and let's get cracking !</p>
+          </div>
+          <div>
+          <AiFillTwitterCircle />
+          <AiFillLinkedin />
+          <AiFillGithub />
+          </div>
         </section>
       </main>
     </div>
